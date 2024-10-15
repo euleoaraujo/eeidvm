a977cf3aa1f845a06e603c4102c7c8d2a7feb9c5
Leandro Araújo <169188222+euleoaraujo@users.noreply.github.com>
2024-10-15 02:12:04 -0300

    full

---
+++ script.js
@@ -0,0 +1,16 @@
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => {
+        elemento.innerHTML += letra;
+      }, 75 * i);
+    });
+  }
+  
+  const titulo = document.querySelector('.titulo');
+  
+  typeWriter(titulo);
+  
+  const buttons = document.querySelectorAll('.cascade-button');
+  
